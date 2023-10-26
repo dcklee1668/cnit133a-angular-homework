6b95f50a122e95defcb9b346e25bfa42a09c138b
David-Lee <david.lee@coxautoinc.com>
2023-10-26 01:50:04 -0700

    Removed unnecessary code.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { MydataService } from '../mydata.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  members: any;
-
-  constructor(private mydata: MydataService) { }
-
-  ngOnInit(): void {
-    this.mydata.getData().subscribe(mydata => {
-      this.members = mydata
-      console.log(this.members)
-    })
-  }
-}
+export class HomeComponent {}
